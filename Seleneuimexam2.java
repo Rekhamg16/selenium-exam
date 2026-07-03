03476d7df41fb1bd2161d60b3cafbb6b44676748
Rekha <rekha.m.gaidhani@gmail.com>
2026-07-03 09:01:53 +0530

    selenium exam 2 paper

---
+++ Seleneuimexam2.java
@@ -0,0 +1,32 @@
+package maven1;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Seleneuimexam2 {
+	public static void main(String[] args) throws InterruptedException {
+		ChromeDriver driver =new ChromeDriver();
+	       driver.get("https://pharmeasy.in/");
+	       Thread.sleep(3000);
+	       driver.manage().window().maximize();
+	       WebElement s1= driver.findElement(By.xpath("//div[@class='typeahead-lib-Typeahead-module_placeholderContent__8HePU']"));
+	       s1.click();
+	       WebElement s2= driver.findElement(By.xpath("//input[@placeholder='Search medicines/Healthcare products']"));
+	       s2.sendKeys("PCM");
+	       Thread.sleep(2000);
+	       List <WebElement> s3 = driver.findElements(By.xpath("//div[@class='typeahead-lib-Typeahead-module_resultsWrapper__mWYqe typeahead-lib-MedicineSearch-module_resultsWrapper__9qkEq']/div/div"));
+	       Thread.sleep(2000);
+	       s3.get(4).click(); //3rd selctions showing out of stock,so selected 4th option
+	       Thread.sleep(2000);
+	       WebElement s4= driver.findElement(By.id("proceed"));
+	       s4.click();
+	       List <WebElement> s5 = driver.findElements(By.xpath(" //div[@role='listbox']/div"));
+	       s5.get(1).click();
+	       Thread.sleep(2000);
+	      WebElement s6= driver.findElement(By.xpath("(//button[@id='proceed'])[15]"));
+	       s6.click();        
+}
+}
